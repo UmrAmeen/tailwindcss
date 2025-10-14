cd2936c13f40a0ea3aae7d0d42a274fdcbe81116
umarameen <umarameen026@gmail.com>
2025-10-14 12:15:26 +0530

    small change

--- app/shopingCart/shopingCart.tsx
+++ app/shopingCart/shopingCart.tsx
@@ -7,28 +7,24 @@ import {
   decreaseItem,
 } from "../shopingCartAction";
 import { useState } from "react";
-import { useRouter } from "next/navigation";
 
 interface RowType {
   [key: string]: any;
 }
 
 export default function ShoppingCart({ cart }: { cart: RowType[] }) {
-  const router = useRouter();
   const [loading, setLoading] = useState(false);
+
   const handleAdd = async (id: number) => {
     await addItem(id);
-    router.refresh();
   };
 
   const handleDecrease = async (id: number) => {
     await decreaseItem(id);
-    router.refresh();
   };
 
   const handleRemove = async (id: number) => {
     await removeItem(id);
-    router.refresh();
   };
 
   const handleBuy = async () => {
@@ -36,7 +32,7 @@ export default function ShoppingCart({ cart }: { cart: RowType[] }) {
     if (!confirmed) return;
     setLoading(true);
     await buyCart(cart);
-    router.refresh();
+
     setLoading(false);
   };
   const total = cart.reduce(
@@ -47,64 +43,65 @@ export default function ShoppingCart({ cart }: { cart: RowType[] }) {
   return (
     <div className="space-y-4">
       {cart.length === 0 ? (
-        <p className="text-red-500 font-bold">Your cart is empty !! .</p>
+        <p className="text-red-500 font-bold">Your cart is empty!!</p>
       ) : (
         <>
           {cart.map((item: RowType) => (
             <div
               key={item.id}
-              className="flex items-center justify-between p-4 border rounded-md shadow-sm"
+              className="flex flex-col md:flex-row items-start md:items-center justify-between p-4 border rounded-md shadow-sm gap-4"
             >
-              <div className="flex items-center gap-4">
+              <div className="flex flex-[2] items-center gap-4">
                 <img
                   src={item.base64Image}
                   alt={item.product_name}
-                  className="w-[64px] h-16 object-cover rounded"
+                  className="w-[84px] h-auto object-cover rounded"
                 />
-                <div>
-                  <p className="font-semibold">{item.product_name}</p>
-                  <p className="text-sm text-green-600">
-                    Price: {item.price.toFixed(2)}
-                  </p>
-
-                  <p className="text-sm text-green-600">
-                    Total: {(item.price * item.quantity).toFixed(2)}
-                  </p>
-
-                  <div className="flex items-center mt-1 gap-2">
-                    <button
-                      onClick={() => handleDecrease(item.id)}
-                      className={`px-2 py-1 border rounded text-sm ${
-                        item.quantity <= 1
-                          ? "bg-gray-600 text-gray-500 cursor-not-allowed"
-                          : " hover:bg-gray-100"
-                      }`}
-                      disabled={item.quantity <= 1}
-                    >
-                      -
-                    </button>
-                    <span>{item.quantity}</span>
-                    <button
-                      onClick={() => handleAdd(item.id)}
-                      className="px-2 py-1 border rounded text-sm  hover:bg-gray-100"
-                    >
-                      +
-                    </button>
-                  </div>
-                </div>
+                <p className="font-medium">{item.product_name}</p>
               </div>
-              <button
-                onClick={() => handleRemove(item.id)}
-                className="text-red-600 hover:text-red-800 hover:bg-gray-100 text-sm font-medium"
-              >
-                Remove
-              </button>
+
+              <div className="flex-1 flex items-center gap-2">
+                <button
+                  onClick={() => handleDecrease(item.id)}
+                  className={`px-2 py-1 border rounded text-sm ${
+                    item.quantity <= 1
+                      ? "bg-gray-300 text-gray-500 cursor-not-allowed"
+                      : "hover:bg-gray-100"
+                  }`}
+                  disabled={item.quantity <= 1}
+                >
+                  -
+                </button>
+                <span>{item.quantity}</span>
+                <button
+                  onClick={() => handleAdd(item.id)}
+                  className="px-2 py-1 border rounded text-sm hover:bg-gray-100"
+                >
+                  +
+                </button>
+              </div>
+              <div className="flex-1 text-green-600 font-medium">
+                Price: ${item.price.toFixed(2)}
+              </div>
+
+              <div className="flex-1 text-green-600 font-medium">
+                Total: ${(item.price * item.quantity).toFixed(2)}
+              </div>
+
+              <div className="w-[80px]">
+                <button
+                  onClick={() => handleRemove(item.id)}
+                  className="text-red-600 hover:text-red-800 hover:bg-gray-100 text-sm font-medium"
+                >
+                  Remove
+                </button>
+              </div>
             </div>
           ))}
 
           <div className="flex justify-between items-center mt-6 border-t pt-4">
             <p className="text-xl font-semibold text-red-400">
-              Total: {total.toFixed(2)}
+              Total: ${total.toFixed(2)}
             </p>
 
             <button
