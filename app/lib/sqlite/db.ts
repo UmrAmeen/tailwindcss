4ef62812be4a516dda7212c99ed0ed85ed8f2bae
Umar <umarameen026@gmail.com>
2025-11-17 13:16:31 +0530

    small change

--- app/lib/sqlite/db.ts
+++ app/lib/sqlite/db.ts
@@ -1,5 +1,5 @@
 import Database from "better-sqlite3";
-const db: Database = new Database("C:\\Users\\user\\products.db");
+const db: Database = new Database(process.env.DB_NAME);
 // const db = new Database("C:\\database\\product.db");
 db.pragma("journal_mode = WAL");
 
--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ export default function HomePage() {
           className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
         >
           Go to Dashboard
+          {/* {process.env.NEXT_PUBLIC_DB_PASSWORD} */}
         </Link>
       </main>
     </div>
