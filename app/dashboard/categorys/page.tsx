0208f9adab643fda3617fb4b4ea48136f199087b
Umar <umarameen026@gmail.com>
2025-12-20 12:23:34 +0530

    smallchange

--- app/dashboard/categorys/page.tsx
+++ app/dashboard/categorys/page.tsx
@@ -1,34 +1,32 @@
-import { supabase } from "@/app/lib/supabaseClient";
+import db from "@/app/lib/db/db";
 import CategoryList from "./categoryList";
+import { category, images } from "@/drizzle/schema";
+import { eq, isNull } from "drizzle-orm";
+
 
 export default async function Categorys() {
-  const { data, error } = await supabase
-    .from("category")
-    .select(
-      `
-      *,
-      images (
-        id,
-        url
-      )
-    `
-    )
-    .is("parent_id", null);
+  const categoryRows = await db
+    .select()
+    .from(category)
+    .leftJoin(images, eq(category.imageId, images.id))
+    .where(isNull(category.parentId));
 
-  if (error) {
-    console.error("Error fetching categories:", error);
-    return <p>Error loading categories</p>;
-  }
-  console.log("data", data);
+  const rowsWithBase64Images = categoryRows.map((row) => {
+    const base64Image = row.images?.image
+      ? `data:image/jpeg;base64,${Buffer.from(
+          row.images.image as Buffer
+        ).toString("base64")}`
+      : null;
 
-  if (!data || data.length === 0) {
-    return <p>No categories found.</p>;
-  }
+    return {
+      ...row,
+      base64Image,
+    };
+  });
 
-  const categoryRows = data.map((row: any) => ({
-    ...row,
-    imageUrl: row.images?.url || "/placeholder.png",
-  }));
-
-  return <CategoryList categoryRows={categoryRows} />;
+  return (
+    <>
+      <CategoryList categoryRows={rowsWithBase64Images} />
+    </>
+  );
 }
