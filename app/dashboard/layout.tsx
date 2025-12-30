a395aa46b8ccb0f524766b65882337f333d0f1f8
umarameen <umarameen026@gmail.com>
2025-12-30 11:26:42 +0530

    Moved files and updated paths

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,8 @@
-"use server";
 import SideBar from "./sidebar";
 import NavBar from "./navBar";
-
 import { supabase } from "../lib/supabaseClient";
-
 import Login from "../loginForm/page";
+
 
 export default async function DashboardLayout({
   children,
@@ -22,10 +20,12 @@ export default async function DashboardLayout({
   return (
     <div className="h-screen flex flex-col">
       <NavBar />
+
       <div className="flex flex-1">
         <div className="w-64 bg-gradient-to-r from-blue-400 to-purple-400 text-white">
           <SideBar />
         </div>
+
         <main className="flex-1 p-4 overflow-auto bg-gradient-to-r from-green-300 to-red-300">
           {children}
         </main>
--- app/dashboard/products/[productSlug]/addToCartButton.tsx
+++ app/dashboard/products/[productSlug]/addToCartButton.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import { addToCart } from "../addtocart";
 
 export default function AddToCartButton({
@@ -12,15 +13,23 @@ export default function AddToCartButton({
   const [quantity, setQuantity] = useState(cartQuantity || 1);
   const [loading, setLoading] = useState(false);
   const [status, setStatus] = useState("");
+  const router = useRouter();
 
   const handleIncrease = () => {
-    setQuantity((quantity) => quantity + 1);
+    setQuantity((q) => q + 1);
   };
 
   const handleAddToCart = async () => {
     setLoading(true);
     const result = await addToCart(productId, quantity);
-    setStatus(result.success ? " Cart updated" : " Error");
+
+    if (result.success) {
+      setStatus("Cart updated");
+      router.refresh();
+    } else {
+      setStatus("Error");
+    }
+
     setLoading(false);
   };
 
@@ -31,7 +40,7 @@ export default function AddToCartButton({
       <button
         onClick={handleIncrease}
         disabled={loading}
-        className="px-3 py-2 bg-gray-900 text-white rounded hover:bg-gray-700 disabled:opacity-50"
+        className="px-3 py-2 bg-gray-900 text-white rounded"
       >
         +
       </button>
@@ -39,12 +48,12 @@ export default function AddToCartButton({
       <button
         onClick={handleAddToCart}
         disabled={loading}
-        className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-800 disabled:opacity-50"
+        className="px-4 py-2 bg-blue-600 text-white rounded"
       >
         {loading ? "Submitting..." : "Add to Cart"}
       </button>
 
-      {status && <p className="text-sm mt-2">{status}</p>}
+      {status && <p className="text-sm">{status}</p>}
     </div>
   );
 }
