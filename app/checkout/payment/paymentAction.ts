cb3cc1ff833fae7c75b5e5e86bed4e2b7d40c31f
Umar <umarameen026@gmail.com>
2026-01-29 12:25:18 +0530

    small change

--- app/checkout/payment/paymentAction.ts
+++ app/checkout/payment/paymentAction.ts
@@ -30,13 +30,7 @@ export async function handlePayment() {
   }
 
   const lineItems = cartItems.map((item) => {
-<<<<<<< HEAD
-    if (!item.price) {
-      throw new Error("Product price missing");
-    }
-=======
     if (!item.price) throw new Error("Product price missing");
->>>>>>> c2686dc32079c84c7b6c1fb99fed578dda6519f9
 
     return {
       price_data: {
